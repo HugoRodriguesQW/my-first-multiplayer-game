c2d8c62a432155c88eddf955e6163d2c909c0cf6
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2020-10-20 19:51:43 +0000

    Update files

--- playground/server.js
+++ playground/server.js
@@ -1,1 +1,5 @@
-console.log("Iniciando Server.js");
+console.log("Server: Starting...")
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
