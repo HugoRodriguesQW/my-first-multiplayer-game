56a97ff06daa8c1de5882b68ecb2fd5948b61322
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-02-08 18:36:13 +0000

    add a basic interface

--- playground/public/js/render.js
+++ playground/public/js/render.js
@@ -26,6 +26,8 @@ export default function makeRender(state, playerId)
     }
 
     const screen = {
+
+        /// Particles ///
         addParticle (name, particle) {
         if(this[name] === undefined) {
             this[name] = {particle: {}}
@@ -39,27 +41,27 @@ export default function makeRender(state, playerId)
 
         updateParticles (id) {
         if(this[id]){
-        const emmiter = this[id].particle
-        for(const p in emmiter) {
-            const {x,y,w,h,src, rate} = emmiter[p]
+            const emmiter = this[id].particle
+            for(const p in emmiter) {
+                const {x,y,w,h,src, rate} = emmiter[p]
 
-            ctx.save()
-            ctx.globalAlpha = emmiter[p].lifetime
-            ctx.drawImage(resources.effects[src], x - (w/2), y - (h/2), w, h)
-            ctx.globalAlpha = 1
-            ctx.restore()
+                ctx.save()
+                ctx.globalAlpha = emmiter[p].lifetime
+                ctx.drawImage(resources.effects[src], x - (w/2), y - (h/2), w, h)
+                ctx.globalAlpha = 1
+                ctx.restore()
 
-            emmiter[p].lifetime -= rate + (Math.random()/10)
-            const sizeRate = Math.random() < .5? 0.1 : -0.1
-            emmiter[p].w += sizeRate
-            emmiter[p].h += sizeRate
-            emmiter[p].x += emmiter[p].speed.x
-            emmiter[p].y += emmiter[p].speed.y
+                emmiter[p].lifetime -= rate + (Math.random()/10)
+                const sizeRate = Math.random() < .5? 0.1 : -0.1
+                emmiter[p].w += sizeRate
+                emmiter[p].h += sizeRate
+                emmiter[p].x += emmiter[p].speed.x
+                emmiter[p].y += emmiter[p].speed.y
 
-            if(emmiter[p].lifetime < 0) {
-                delete emmiter[p]
+                if(emmiter[p].lifetime < 0) {
+                    delete emmiter[p]
+                }
             }
-        }
         }
         }
     }
@@ -156,7 +158,7 @@ export default function makeRender(state, playerId)
                     screen.addParticle(id, {
                     x: 0, y: -ref.y + 2,
                     w: 12 + Math.random()*1.5 , h: 12 + Math.random()*1.5,
-                    src: 'white-fire', rate: 0.01, speed: {x:0, y: 1}
+                    src: 'white-fire', rate: 0.0001, speed: {x:0, y: 1}
                     })
             }
         }
@@ -169,27 +171,21 @@ export default function makeRender(state, playerId)
     function renderPlayers ()
     {
         const players = state.players
-        for(const p in players) {
+        for(const id in players) {
 
             ctx.save()
             const width = 143/4
             const height = 286/4
-            ctx.translate(
-                players[p].x + width/2, players[p].y + height/2,
-            )
 
-            ctx.rotate(players[p].rot*Math.PI/180)
+            ctx.translate(players[id].x + width/2, players[id].y + height/2)
+            ctx.rotate(players[id].rot*Math.PI/180)
 
-            screen.updateParticles(p)
-
-
+            screen.updateParticles(id)
             ctx.drawImage(resources.rockets.simple,
-            -width/2, -height/2,
-            width, height
-            )
+            -width/2, -height/2, width, height)
 
-            Object.keys(players[p].particleEmitters).map( (type) => {
-                renderParticles(type, p)
+            Object.keys(players[id].particleEmitters).map( (type) => {
+                renderParticles(type, id)
             })
 
             ctx.restore ()
@@ -197,13 +193,39 @@ export default function makeRender(state, playerId)
         }
     }
 
+    function renderInterface (from) {
+        ctx.save()
+
+        const render = {
+            text({value, style, pos}){
+                for(const prop in style){
+                    ctx[prop] = style[propcd]
+                }
+                ctx.fillText(value, camera.x + pos[0], camera.y + pos[1])
+            },
+        }
+
+        render.text({value: from.x.toFixed(2), pos: [25, 20], style:{direction: 'ltr', fillStyle: 'red', font: '13px Arial'}})
+        render.text({value: from.y.toFixed(2), pos: [25, 35], style:{direction: 'ltr', fillStyle: 'green', font: '13px Arial'}})
+        render.text({value: 'X:', pos: [10, 20], style:{direction: 'ltr', fillStyle: 'red', font: '13px Arial'}})
+        render.text({value: 'Y:', pos: [10, 35], style:{direction: 'ltr', fillStyle: 'green', font: '13px Arial'}})
+
+        for (const p in state.players) {
+            render.text({value: p, pos: [canvas.width-20, 20 + (15 * Object.keys(state.players).indexOf(p))], style:{direction: 'rtl', fillStyle: 'white', font: '13px Arial'}})
+        }
+
+        ctx.restore()
+    }
+
     function update() {
 
         if(state.players[playerId]) {
-        ctx.clearRect(camera.x, camera.y, camera.x + canvas.width *2 , camera.y + canvas.height *2)
+
+        ctx.clearRect(camera.x, camera.y, canvas.width, canvas.height)
         camera.follow(state.players[playerId])
         ctx.setTransform(1, 0, 0, 1, -camera.x, -camera.y)
 
+        renderInterface(state.players[playerId])
         renderPlayers()
 
         }
@@ -229,7 +251,6 @@ export default function makeRender(state, playerId)
                         const keys = Object.keys(resource)
 
                         if(keys.indexOf(res) === keys.length -1 ) {
-                            console.groupEnd()
                             accept()
                         }
                     }
